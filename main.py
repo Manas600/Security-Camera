f0ec7eaaf491e4431f2a87e8d1c06afb4247527b
Manas Hardikar <hardikarmanas600@gmail.com>
2023-08-06 13:50:25 +0530

    detecting faces and body

--- main.py
+++ main.py
@@ -4,8 +4,25 @@ import datetime
 
 cap = cv.VideoCapture(0)
 
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_fullbody.xml")
+
+recording = True
+
 while True:
     _, frame = cap.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
+
+    if len(faces) + len(bodies) > 0:
+        recording = True
+
+    # for (x,y,width,height) in faces:
+    #     cv.rectangle(frame,(x,y),(x+width,y+height),(255,0,0),3)
+    # for (x,y,width,height) in bodies:
+    #     cv.rectangle(frame,(x,y),(x+width,y+height),(0,255,0),3)
 
     cv.imshow("Camera", frame)
 
