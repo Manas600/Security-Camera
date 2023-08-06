7e84e72f633d409e33b880b1fe029029100e15db
Manas Hardikar <hardikarmanas600@gmail.com>
2023-08-06 14:42:46 +0530

    Complete

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ cap = cv.VideoCapture(0)
 face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
 body_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_fullbody.xml")
 
-recording = True
+detection = False
+detection_start_time = None
+SECONDS_TO_RECORD_AFTER_DETECTION = 10
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv.VideoWriter_fourcc(*"mp4v")
+out = None
 
 while True:
     _, frame = cap.read()
@@ -17,18 +23,31 @@ while True:
     bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
 
     if len(faces) + len(bodies) > 0:
-        recording = True
+        if not detection:
+            detection = True
+            detection_start_time = time.time()
+            currentTime = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+            out = cv.VideoWriter(f"{currentTime}.mp4", fourcc, 30, frame_size)
+            print("Started recording")
+            
+    else:
+        if detection:
+            if time.time() - detection_start_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                out.release()
+                print("Stopped recording")
+                print(f"Video saved as {currentTime}")
 
-    # for (x,y,width,height) in faces:
-    #     cv.rectangle(frame,(x,y),(x+width,y+height),(255,0,0),3)
-    # for (x,y,width,height) in bodies:
-    #     cv.rectangle(frame,(x,y),(x+width,y+height),(0,255,0),3)
+    if detection:
+        out.write(frame)
 
     cv.imshow("Camera", frame)
 
     if cv.waitKey(1) == ord("q"):
         break
 
+if detection:
+    out.release()
 
-cap.release
-cv.destroyAllWindows()    
+cap.release()
+cv.destroyAllWindows()
